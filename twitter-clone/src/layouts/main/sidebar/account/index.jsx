3a598cbea4770e2c027b73739e8bd5bb8d0a305b
Ugur BOZKURT <ugur.bozkurt@ogr.deu.edu.tr>
2023-11-08 01:15:16 +0300

    Store auth account created

--- twitter-clone/src/layouts/main/sidebar/account/index.jsx
+++ twitter-clone/src/layouts/main/sidebar/account/index.jsx
@@ -7,9 +7,8 @@ export default function Account(){
         <div className="mt-auto">
             <Popover>
                     <Popover.Button className="my-3 rounded-full p-3 flex w-full text-left hover:bg-[#EFF3F41A] " >
-                        
                         <img src={account.avatar} className="w-10 h-10 rounded-full"></img>
-                       <div className="mx-3 text-[15px]">
+                       <div className="mx-5 text-[15px]">
                         <h6 className="font-bold">{account.fullName}</h6>
                         <div className="text-[#71767b]">
                         @{account.username}</div>
