d66620064aa26347d67ef35cbcd91e32e82fddb0
Ugur BOZKURT <69724299+ugurbozkurt@users.noreply.github.com>
2023-12-04 12:03:59 +0300

    Delete for path deatils page

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home"
-import Detail from './pages/detail'
-import Navbar from './components/navbar'
+
 import Footer from './components/Footer'
 import PageContainers from './containers/PageContainers'
 import { useSelector } from 'react-redux'
@@ -18,7 +17,6 @@ export default function App() {
           <Search/>
           <Routes>
             <Route path="/home" element={<Home />} />
-            <Route path="/detail" element={<Detail />} />
           </Routes>
           { drawer && <Card/>}
           <Footer />
--- e-commerce/src/pages/Detail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Deatil = () => {
-  return (
-    <div>Deatil</div>
-  )
-}
-
-export default Deatil
