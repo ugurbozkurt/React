7459428542f88a4e3cc32f72756950c76d6071ff
Ugur BOZKURT <69724299+ugurbozkurt@users.noreply.github.com>
2023-12-06 01:57:19 +0300

    / path

--- e-commerce/src/components/navbar/index.jsx
+++ e-commerce/src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <Link to="/home">Home</Link>
+    <Link to="/">Home</Link>
     <Link to="/detail">Deatil</Link>
    
     </>
