f3503f55a3c072f8473e780871cff02ba37bc862
Ugur BOZKURT <69724299+ugurbozkurt@users.noreply.github.com>
2023-12-09 16:08:48 +0300

    Total Price added to cart

--- e-commerce/src/components/card/index.jsx
+++ e-commerce/src/components/card/index.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { SiWearos } from "react-icons/si";
 
 import { addCard } from '../../redux/actions/card' 
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch} from 'react-redux'
+
+
 const Card = ({products}) => {
   const dispatch = useDispatch()
-
   const addCards = ()=>{
-   dispatch({type:"DRAWER",payload:true})
    dispatch(addCard(products.id,1))
+   dispatch({type:"DRAWER",payload:true})
+   
   }
   return (
     <div  className='w-1/5 border-2 cursor-pointer p-4 rounded-lg gap-3 m-2 flex flex-col items-center text-center transition color hover:border-[#000]'>
@@ -16,7 +18,7 @@ const Card = ({products}) => {
         <div className='font-bold h-16'>{(products?.title).substring(0,45)}..</div>
         <div>{(products?.description).substring(0,45)}...</div>
         <div className='font-bold text-lg mb-4'>{products?.price} TL</div>
-        <button onClick={addCards} type='button' className='w-[130px]  p-2 relative flex items-center justify-center border-b-2 border-black transition-colors rounded-xl text-black hover:bg-black hover:text-white '>Add to cart <SiWearos  size={20} className='ml-2' /></button>
+        <button onClick={addCards} type='button' className='w-[130px] bg-black text-[#FFF]  p-2 relative flex items-center justify-center  border-black transition-colors rounded-xl text-black hover:border-b-2 hover:bg-white hover:text-black '>Add to cart <SiWearos  size={20} className='ml-2' /></button>
     </div>
   )
 }
--- e-commerce/src/components/cart/index.jsx
+++ e-commerce/src/components/cart/index.jsx
@@ -1,15 +1,32 @@
 import { React, useState, useEffect } from 'react'
 import { RiCloseCircleFill } from "react-icons/ri";
 import { useDispatch, useSelector } from 'react-redux';
-import { MdRemoveShoppingCart } from "react-icons/md";
 import { removeCard } from '../../redux/actions/card';
+import { IoTrashOutline } from "react-icons/io5";
+
 const Cart = () => {
   const dispatch = useDispatch();
   const [color, setColor] = useState(false);
+
   const { cardItems } = useSelector(state => state.card)
-  const deleteCard = (id)=>{
-    dispatch(removeCard(id))
+  const carL = cardItems?.length
+  
+
+
+  let totalPrice = 0
+  for(let i = 0; i<cardItems?.length; i++)
+  {
+    totalPrice += cardItems?.[i].price
   }
+  
+  const deleteCard = (id) => {
+    dispatch(removeCard(id))
+    if (carL <=1) {
+      handleCloseClick()
+    }
+
+  }
+
   useEffect(() => {
     const mycart = document.getElementById('mycart');
     const root = document.getElementById('root');
@@ -20,43 +37,47 @@ const Cart = () => {
       mycart.style.backgroundColor = "white";
       mycart.style.color = "black";
     }
-  }, [color]);
-
+  }, [color, cardItems]);
   const handleCloseClick = () => {
     const mycart = document.getElementById('mycart');
     mycart.classList.add('closing');
     setTimeout(() => {
       dispatch({ type: "DRAWER", payload: false });
-    }, 300); 
+    }, 300);
   };
-
+ 
+ 
   return (
     <div
       id="mycart"
-      className='w-1/3 h-full border p-3 fixed top-0 right-0 z-44'
+      className='w-1/3 h-full border  overflow-y-auto p-3  fixed top-0 right-0 z-44'
     >
       <div className='flex items-center justify-between'>
-        <h1 className='text-2xl'>My cart :</h1>
+        <h1 className='text-2xl'>My cart : {carL}</h1>
         <RiCloseCircleFill
           onClick={handleCloseClick}
           size={25}
           className='cursor-pointer'
         />
-
       </div>
-    {
-      cardItems?.map((item,index)=>(
-        <div key={index} className='flex mt-5 h-28 items-center justify-between border-b pb-7 px-7 py-5'>
-        <img src={item?.image} className='h-24'></img>
-        <div className='w-44'>
-          <div className='font-bold text-sm'>{item?.title}</div>
-          <div className='opacity-70 text-xs'>{item?.description.substring(0,100)}</div>
-        </div>
-        <div className='font-bold text-lg'>{item?.price*item?.qty} TL</div>
-        <MdRemoveShoppingCart onClick={()=> deleteCard(item?.id)} fill='white' className='w-[30px] cursor-pointer text-center h-[30px] bg-red-500 p-2 rounded-full transition-colors hover:bg-black' />
-      </div>
-      ))
-    }
+      {
+        cardItems?.map((item, index) => (
+          <div key={index} className='flex mt-5 h-28 items-center justify-between border-b pb-7 px-7 py-5'>
+            <img src={item?.image} className='h-24'></img>
+            <div className='w-44'>
+              <div className='font-bold text-sm'>{item?.title}</div>
+              <div className='opacity-70 text-xs'>{item?.description.substring(0, 100)}</div>
+            </div>
+            <div className='font-bold text-lg'>{item?.price * item?.qty} TL</div>
+            <IoTrashOutline onClick={() => deleteCard(item?.id)} fill='white' className='w-[30px] cursor-pointer text-center h-[30px] p-1 rounded-full transition-colors hover:bg-black hover:text-[#FFF]' />
+          </div>
+        ))
+      }
+      {
+        totalPrice>0 && (
+          <div className='font-bold text-2xl text-right'>Total Price : {totalPrice} TL</div>
+        )
+      }
     </div>
   );
 };
